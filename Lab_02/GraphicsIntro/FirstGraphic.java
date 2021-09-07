32c6fbd119417e1c0b689e9152995522aa49d9da
manlol33633 <51886476+manlol33633@users.noreply.github.com>
2021-09-07 17:34:42 -0500

    Update FirstGraphic.java

--- Lab_02/GraphicsIntro/FirstGraphic.java
+++ Lab_02/GraphicsIntro/FirstGraphic.java
@@ -3,12 +3,13 @@ import javax.swing.*;
 
 public class FirstGraphic extends JFrame {
 	public static final int WIDTH = 1200;
-	public static final int HEIGHT = 400;
+	public static final int HEIGHT = 480;
 	
 	public FirstGraphic(String framename) {
 		super(framename);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(WIDTH, HEIGHT);
+		add(new FirstGraphicPanel());
 		setVisible(true);
 	}
 }
